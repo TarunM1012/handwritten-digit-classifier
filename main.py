081d319626af10a1db0bbc793a929539ca563524
TarunM1012 <34863641+TarunM1012@users.noreply.github.com>
2025-10-06 22:31:42 -0400

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+#main.py
+
+#import all necessary modules
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torch.optim as optim
+import torch.nn as nn
+
